16dd5bdaa06adf8085daf768352584d7ad109d2a
Peter Eldritch <peter.jirak@gmail.com>
2024-01-21 11:52:50 -0800

    Added a test to test insert_into for inserting ten items into a list in descending order.

--- Working_With_Lists/test_listtools.py
+++ Working_With_Lists/test_listtools.py
@@ -7,5 +7,13 @@ def test_insert_into_starting_with_an_empty_list_ten_items_ascending():
         insert_into(item_list, item)
     assert item_list == [3, 4, 5, 7, 25, 31, 45, 75, 80, 93], "Successfully inserted in ascending order ten items into a list"
 
-# if __name__ == '__main__':
-#    test_insert_into_starting_with_an_empty_list_ten_items_ascending()
+def test_insert_into_starting_with_an_empty_list_ten_items_descending():
+    items_to_insert = [ 31, 5, 75, 3, 25, 45, 80, 4, 93, 7 ]
+    item_list = []
+    for item in items_to_insert:
+        insert_into(item_list, item, ascending=False)
+    assert item_list == [93, 80, 75, 45, 31, 25, 7, 5, 4, 3], "Successfully inserted in descending order ten items into a list"
+
+#if __name__ == '__main__':
+#   test_insert_into_starting_with_an_empty_list_ten_items_ascending()
+#    test_insert_into_starting_with_an_empty_list_ten_items_descending()
