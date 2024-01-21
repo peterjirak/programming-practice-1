1280f1adbc3c8e1ef4a65890cc338b9d95fa9068
Peter Eldritch <peter.jirak@gmail.com>
2024-01-21 12:43:41 -0800

    insert_into is used to build an ordered list, starting with an ordered list, possibly empty, items are added to it one at a time in order (ascending or descending depending on the value of the ascending flag). It uses the bisection techique to find the index in which to insert the item. There is a left index and a right index if the item to add is greater than item_list[left] and the item to add is less than item_list[right], then the midpoint is calculated mid = Math.floor((right + left)/2). When the midpoint is calculated using mid = Math.floor((right + left)/2), left will never equal right and the closest they will get is left == right - 1.
    Previously I had two termination conditions in my recursive insert_into
    function:
    1. left == right
    2. left == right - 1
    
    I merged these together to:
    
        right - left <= 1

--- Working_With_Lists/listtools.py
+++ Working_With_Lists/listtools.py
@@ -58,44 +58,17 @@ def insert_into(item_list, item, left=None, right=None, ascending=True):
     if left > right:
         raise ValueError(f'Bad invocation of insert_into - left == {left}, ' +
                          f'right == {right} - left cannot be greater than right.')
-    elif left == right:
+    elif right - left <= 1:
         if len(item_list) == 0:
             item_list.append(item)
             return
-        if ascending:
-            if item >= item_list[right]:
-                if len(item_list) - 1 == right:
-                    item_list.append(item)
-                    return
-                else:
-                    shift_right(item_list, right + 1)
-                    item_list[right + 1] = item
-                    return
-            else:
-                shift_right(item_list, right)
-                item_list[right] = item
-                return
-        else:
-            if item <= item_list[right]:
-                if len(item_list) - 1 == right:
-                    item_list.append(item)
-                    return
-                else:
-                    shift_right(item_list, right + 1)
-                    item_list[right + 1] = item
-                    return
-            else:
-                shift_right(item_list, right)
-                item_list[right] = item
-                return
-    elif right - left == 1:
-        if ascending:
+        elif ascending:
             if item <= item_list[left]:
                 shift_right(item_list, left)
                 item_list[left] = item
                 return
             elif item >= item_list[right]:
-                if right == len(item_list) - 1:
+                if len(item_list) - 1 == right:
                     item_list.append(item)
                     return
                 else:
@@ -112,7 +85,7 @@ def insert_into(item_list, item, left=None, right=None, ascending=True):
                 item_list[left] = item
                 return
             elif item <= item_list[right]:
-                if right == len(item_list) - 1:
+                if len(item_list) - 1 == right:
                     item_list.append(item)
                     return
                 else:
