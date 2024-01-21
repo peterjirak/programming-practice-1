1708c524ca45a98dd4f4e8e4412352d712c038e6
Peter Eldritch <peter.jirak@gmail.com>
2024-01-21 11:01:46 -0800

    Fixed a defect in the starting condition for the call into insert_into

--- Working_With_Lists/test_listtools.py
+++ Working_With_Lists/test_listtools.py
@@ -4,5 +4,8 @@ def test_insert_into_starting_with_an_empty_list_ten_items_ascending():
     items_to_insert = [ 31, 5, 75, 3, 25, 45, 80, 4, 93, 7 ]
     item_list = []
     for item in items_to_insert:
-        insert_into(item_list, item, 0, len(item_list) - 1, True)
+        insert_into(item_list, item, 0, len(item_list) - 1 if len(item_list) - 1 > 0 else 0, True)
     assert item_list == [3, 4, 5, 7, 25, 31, 45, 75, 80, 93], "Successfully inserted in ascending order ten items into a list"
+
+# if __name__ == '__main__':
+#    test_insert_into_starting_with_an_empty_list_ten_items_ascending()
