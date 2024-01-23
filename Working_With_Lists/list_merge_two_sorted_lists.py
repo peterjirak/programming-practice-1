ed7f5ed6877c25a554c42f3e98b1fc20d3711659
Peter Eldritch <peter.jirak@gmail.com>
2024-01-23 09:41:47 -0800

    Modified list_merge_two_sorted_lists to support empty lists

--- Working_With_Lists/list_merge_two_sorted_lists.py
+++ Working_With_Lists/list_merge_two_sorted_lists.py
@@ -1,4 +1,34 @@
 """
+is_ascending(item_list)
+
+ARGUMENTS
+1. item_list
+
+RETURNS
+True if and only if item_list is ordered in ascending order
+"""
+def is_ascending(item_list):
+    for i in range(len(item_list) - 1):
+        if item_list[i] > item_list[i + 1]:
+            return False
+    return True
+
+"""
+is_descending(item_list)
+
+ARGUMENTS
+1. item_list
+
+RETURNS
+True if and only if item_list is ordered in descending order
+"""
+def is_descending(item_list):
+    for i in range(len(item_list) - 1):
+        if item_list[i] < item_list[i + 1]:
+            return False
+    return True
+
+"""
 list_merge_two_sorted_lists(item_list1, item_list2)
 
 ARGUMENTS
@@ -18,14 +48,58 @@ def list_merge_two_sorted_lists(item_list1, item_list2):
     i2 = 0
     len1 = len(item_list1)
     len2 = len(item_list2)
+    if len1 == 0 and len2 == 0:
+        return []
+    ascending = None
+    ascending1 = is_ascending(item_list1)
+    ascending2 = is_ascending(item_list2)
+    descending1 = is_descending(item_list1)
+    descending2 = is_descending(item_list2)
+    if len1 == 0:
+        if ascending2:
+            ascending = True
+        elif descending2:
+            ascending = False
+        else:
+            raise ValueError('item_list2 is not sorted')
+    elif len2 == 0:
+        if ascending1:
+            ascending = True
+        elif descending1:
+            ascending = False
+        else:
+            raise ValueError('item_list1 is not sorted')
+    elif ascending1:
+        if not ascending2:
+            raise ValueError('item_list1 is ascending but item_list2 is not ascending')
+        else:
+            ascending = True
+    elif descending1:
+        if not descending2:
+            raise ValueError('item_list1 is descending but item_list2 is not descending')
+        else:
+            ascending = False
+    elif not descending2:
+        raise ValueError('argument lists are not sorted')
+    else:
+        raise ValueError('item_list1 is not sorted')
+
     while i1 < len1 and i2 < len2:
         if i1 < len1 and i2 < len2:
-            if item_list1[i1] <= item_list2[i2]:
-                merged_list.append(item_list1[i1])
-                i1 += 1
+            if ascending:
+                if item_list1[i1] <= item_list2[i2]:
+                    merged_list.append(item_list1[i1])
+                    i1 += 1
+                else:
+                    merged_list.append(item_list2[i2])
+                    i2 += 1
             else:
-                merged_list.append(item_list2[i2])
-                i2 += 1
+                if item_list1[i1] >= item_list2[i2]:
+                    merged_list.append(item_list1[i1])
+                    i1 += 1
+                else:
+                    merged_list.append(item_list2[i2])
+                    i2 += 1
     for j in range(i1, len1):
         merged_list.append(item_list1[j])
     for j in range(i2, len2):
--- Working_With_Lists/test_list_merge_two_sorted_lists.py
+++ Working_With_Lists/test_list_merge_two_sorted_lists.py
@@ -1,8 +1,15 @@
 from list_merge_two_sorted_lists import list_merge_two_sorted_lists
 
-def test_list_merge_two_sorted_lists():
+def test_list_merge_two_sorted_lists_ascending():
     l1 = [ 5, 9, 13, 17, 23, 27, 33, 37, 43, 47 ]
     l2 = [ 3, 7, 15, 15, 15, 16, 17, 18, 19, 23, 27, 29, 31, 36, 39, 41, 45, 48 ]
     merged = list_merge_two_sorted_lists(l1, l2)
     assert merged == [3, 5, 7, 9, 13, 15, 15, 15, 16, 17, 17, 18, 19, 23, 23, 27, 27, 29, 31, 33, 36, 37, 39, 41, 43, 45, 47, 48], (
+          "list_merge_two_sorted_lists correctly merged two lists")
+
+def test_list_merge_two_sorted_lists_descending():
+    l1 = [ 47, 43, 37, 33, 27, 23, 17, 13, 9, 5 ]
+    l2 = [ 48, 45, 41, 39, 36, 31, 29, 27, 23, 19, 18, 17, 16, 15, 15, 15, 7, 3 ]
+    merged = list_merge_two_sorted_lists(l1, l2)
+    assert merged == [48, 47, 45, 43, 41, 39, 37, 36, 33, 31, 29, 27, 27, 23, 23, 19, 18, 17, 17, 16, 15, 15, 15, 13, 9, 7, 5, 3], (
           "list_merge_two_sorted_lists correctly merged two lists")
