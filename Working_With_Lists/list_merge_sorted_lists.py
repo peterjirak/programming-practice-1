8c0721c47482e8b32a7f724cbdc82031ba5144e1
Peter Eldritch <peter.jirak@gmail.com>
2024-01-31 01:07:20 -0800

    Modified merge_sorted_lists to merge ascending or descending lists - note that all lists that merge_sorted_lists is invoked with must be ascending or descending. If merge_sorted_lists is invoked with a combination of ascending and descending lists, then it will throw an exception.

--- Working_With_Lists/list_merge_sorted_lists.py
+++ Working_With_Lists/list_merge_sorted_lists.py
@@ -1,5 +1,7 @@
-def _get_next(indices, lengths, *lists):
-    min_item = None
+from list_merge_two_sorted_lists import is_ascending, is_descending
+
+def _get_next(indices, lengths, *lists, ascending=True):
+    next_item = None
     list_index = None
 
     for i in range(len(lists)):
@@ -7,16 +9,46 @@ def _get_next(indices, lengths, *lists):
         index = indices[i]
         if index >= length:
             continue
-        elif min_item is None or lists[i][index] < min_item:
-            min_item = lists[i][index]
-            list_index = i
+        elif ascending:
+            if next_item is None or lists[i][index] < next_item:
+                next_item = lists[i][index]
+                list_index = i
+        else:
+            if next_item is None or lists[i][index] > next_item:
+                next_item = lists[i][index]
+                list_index = i
     
     if list_index is not None:
         indices[list_index] += 1
 
-    return min_item
+    return next_item
 
 def merge_sorted_lists(*lists):
+    are_ascending = None
+    are_descending = None
+    for i in range(len(lists)):
+        l = lists[i]
+        asc = is_ascending(l)
+        desc = is_descending(l)
+        if not asc and not desc:
+            raise ValueError(f'The list in position {i} is neither ascending nor descending')
+        elif asc:
+            if are_descending:
+                raise ValueError(f'The lists in positions 0 - {i - 1} ' +
+                                 'are descending while the list at position ' +
+                                 f'{i} is ascending.')
+            elif are_descending is None and are_ascending is None:
+                are_ascending = True
+                are_descending = not are_ascending
+        else:
+            if are_ascending:
+                raise ValueError(f'The lists in positions 0 - {i - 1} ' +
+                                 'are ascending while the list at position ' +
+                                 f'{i} is descending.')
+            elif are_descending is None and are_ascending is None:
+                are_descending = True
+                are_ascending = not are_descending
+
     list_count = len(lists)
     lengths = [None] * list_count
     for i in range(len(lists)):
@@ -26,10 +58,10 @@ def merge_sorted_lists(*lists):
     merged = [None] * item_count
     m = 0
     
-    next_item = _get_next(indices, lengths, *lists)
+    next_item = _get_next(indices, lengths, *lists, ascending=are_ascending)
     while next_item is not None:
         merged[m] = next_item
         m += 1
-        next_item = _get_next(indices, lengths, *lists)
+        next_item = _get_next(indices, lengths, *lists, ascending=are_ascending)
     
     return merged
--- Working_With_Lists/test_list_merge_sorted_lists.py
+++ Working_With_Lists/test_list_merge_sorted_lists.py
@@ -5,7 +5,15 @@ def test_merge_sorted_lists_merge_two_ascending_sorted_lists():
     l2 = [ 3, 7, 15, 15, 15, 16, 17, 18, 19, 23, 27, 29, 31, 36, 39, 41, 45, 48 ]
     merged = merge_sorted_lists(l1, l2)
     assert merged == [3, 5, 7, 9, 13, 15, 15, 15, 16, 17, 17, 18, 19, 23, 23, 27, 27, 29, 31, 33, 36, 37, 39, 41, 43, 45, 47, 48], (
-          "merge_sorted_lists correctly merged two lists")
+          "merge_sorted_lists correctly merged two sorted, ascending lists")
+
+def test_merge_sorted_lists_merge_two_descending_sorted_lists():
+    l1 = [47, 43, 37, 33, 27, 23, 17, 13, 9, 5]
+    l2 = [48, 45, 41, 39, 36, 31, 29, 27, 23, 19, 18, 17, 16, 15, 15, 15, 7, 3]
+    merged = merge_sorted_lists(l1, l2)
+    assert merged == [48, 47, 45, 43, 41, 39, 37, 36, 33, 31, 29, 27, 27, 23, 23, 19, 18, 17, 17, 16, 15, 15, 15, 13, 9, 7, 5, 3], (
+           "merge_sorted_lists correctly merged two sorted, descending lists"
+    )
 
 # if __name__ == '__main__':
-#     test_merge_sorted_lists_merge_two_ascending_sorted_lists()
+#     test_merge_sorted_lists_merge_two_descending_sorted_lists()
